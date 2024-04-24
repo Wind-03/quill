dc24b13070460fa885bcaa6f0a12ab8a571df88f
Faisal.yussuf <foyussuf@student.oauife.edu.ng>
2024-04-24 14:13:33 +0100

    new changes

--- src/app/_trpc/client.ts
+++ src/app/_trpc/client.ts
@@ -1,4 +1,5 @@
+import { AppRouter } from '@/trpc';
 import { createTRPCReact } from '@trpc/react-query';
 import { z } from 'zod';
 
-export const trpc = createTRPCReact<>({});
+export const trpc = createTRPCReact<AppRouter>({});
--- src/app/auth-callback/Page.tsx
+++ src/app/auth-callback/Page.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+
 import { useRouter, useSearchParams } from 'next/navigation'
+import { trpc } from '../_trpc/client'
 
-const Page = () => {
-    const router = useRouter()
+const Page = async () => {
+  const router = useRouter()
 
-    const searchParams = useSearchParams()
-    const origin = searchParams.get('origin') 
-    return (
-    <div>page</div>
-  )
+  const searchParams = useSearchParams()
+  const origin = searchParams.get('origin')
+
+  const { data, isLoading } = trpc.authCallback.useQuery()
+
+  if (data?.success) {
+    //user synced to db
+    router.push(origin ? `/${origin}` : '/dashboard')
+  }
 }
 
 export default Page
